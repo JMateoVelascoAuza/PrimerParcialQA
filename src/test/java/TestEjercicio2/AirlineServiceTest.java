ef32d42b54c7ef070e1880269f5213ecfabab6d3
Julio Mateo Velasco Auza <80355690+JMateoVelascoAuza@users.noreply.github.com>
2023-10-04 19:25:14 -0400

    parcial 1 commit Final

--- src/test/java/TestEjercicio2/AirlineServiceTest.java
+++ src/test/java/TestEjercicio2/AirlineServiceTest.java
@@ -3,6 +3,7 @@ import Ejercicio2.AirlineServiceUtils;
 import Ejercicio3.AirlineService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.StringCache;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
@@ -44,24 +45,29 @@ public class AirlineServiceTest {
          }
 
 @Test
-public void testBookFlightOnSunday(){
-
-
-    try (MockedStatic<AirlineServiceUtils>utilities = Mockito.mockStatic(AirlineServiceUtils.class)){
-
+public void testBookFlightOnSunday() {
+    try (MockedStatic<AirlineServiceUtils> utilities = Mockito.mockStatic(AirlineServiceUtils.class)) {
         utilities.when(() -> AirlineServiceUtils.areTicketsAvailable("La Paz", 2)).thenReturn(true);
-       AirlineService airlineService = new AirlineService();
-        String response = airlineService.bookFlight("La Paz", 3, 29, 5, 2023);
+        utilities.when(() -> AirlineServiceUtils.getWeekday(28, 5, 2023)).thenReturn("Sunday");
 
-        Assertions.assertEquals("There are not enough tickets available for La Paz", response);
+        AirlineService airlineService = new AirlineService();
+        String response = airlineService.bookFlight("La Paz", 2, 28, 5, 2023);
 
+        Assertions.assertEquals("On Sunday 28 May 2023, there are 2 tickets available for La Paz", response);
     }
-
-
 }
 
+    @Test
+    public void testBookFlightDifferentMonth() {
+        try (MockedStatic<AirlineServiceUtils> utilities = Mockito.mockStatic(AirlineServiceUtils.class)) {
+            utilities.when(() -> AirlineServiceUtils.areTicketsAvailable("La Paz", 2)).thenReturn(true);
+            utilities.when(() -> AirlineServiceUtils.getWeekday(1, 6, 2023)).thenReturn("Thursday");
 
+            AirlineService airlineService = new AirlineService();
+            String response = airlineService.bookFlight("La Paz", 2, 1, 6, 2023);
 
-
+            Assertions.assertEquals("On Thursday 1 June 2023, there are 2 tickets available for La Paz", response);
+        }
+    }
 
 }
